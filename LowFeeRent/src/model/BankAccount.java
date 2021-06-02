6794b8c81d7845a07b7bf5fde841f056aeebea02
Arthur <nikitinarthur@gmail.com>
2021-06-02 13:07:00 +0200

    v0.3

--- LowFeeRent/src/model/BankAccount.java
+++ LowFeeRent/src/model/BankAccount.java
@@ -1,8 +1,31 @@
 package model;
 
 public class BankAccount {
-	Bank bank;
 	String iban;
 	double amount;
+	
+	public BankAccount(String iban, double amount) {
+		super();
+		this.iban = iban;
+		this.amount = amount;
+	}
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+
 
 }
--- LowFeeRent/src/test/Test.java
+++ LowFeeRent/src/test/Test.java
@@ -1,23 +1,31 @@
 package test;
 
+import java.util.ArrayList;
+
 import com.github.javafaker.Faker;
 
+import model.BankAccount;
+import model.Person;
+
 public class Test {
-	
+
 	public static void main(String[] args) {
-		
+		int n = 0;
+
 		System.out.println("Welcome to LowFeeRent!");
-		
-		
+
 		Faker faker = new Faker();
 
-		String name = faker.name().fullName(); // Miss Samanta Schmidt
-		String surname = faker.name().firstName(); // Emory
-		String lastName = faker.name().lastName(); // Barton
+		ArrayList<Person> lessors = new ArrayList<Person>();
+		ArrayList<Person> tenants = new ArrayList<Person>();
 
-		String streetAddress = faker.address().streetAddress(); // 60018 Sawayn Brooks Suite 449
+		while (n < 10) {
+			String name = faker.name().firstName();
+			String surname = faker.name().lastName();
+			String iban = faker.finance().iban();
+			lessors.add(new Person(name, surname, new BankAccount(iban, Math.random() * 1000)));
+		}
 		
-		
+		System.out.println(lessors);
 	}
-
 }
