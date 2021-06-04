8a25ae15f4f366217cc28824af78cf38622e08d9
Arthur <nikitinarthur@gmail.com>
2021-06-04 09:17:55 +0200

    convert DB to HashMap

--- LowFeeRent/src/test/Test.java
+++ LowFeeRent/src/test/Test.java
@@ -19,10 +19,8 @@ public class Test {
 	private static void generateData() {
 
 		AppDB db = new AppDB();
-
 		Faker faker = new Faker();
 
-		//ArrayList<Lessor> lessors = new ArrayList<Lessor>();
 		System.out.println("---------------- Lessors: ----------------");
 		int n = 1;
 		while (n <= 10) {
@@ -30,32 +28,25 @@ public class Test {
 			lessor.setName(faker.name().firstName());
 			lessor.setSurname(faker.name().lastName());
 			lessor.setBankAccount(new BankAccount(faker.finance().iban(), Utils.randRange(5, 55) * 100));
-
 			Apartment apartment = new Apartment(Utils.randRange(8, 25) * 100, Utils.randRange(10, 20) * 10,
 					Utils.randRange(1, 5), Utils.randRange(1, 3), faker.address().streetAddress(true));
 			apartment.setPersonId(lessor.getId());
 			lessor.setApartmentId(apartment.getId());
 			db.getLessors().put(lessor.getId(), lessor);
 			db.getApartments().put(apartment.getId(), apartment);
-
 			System.out.println("\n#" + n + " " + lessor);
 			System.out.println(apartment);
-
 			n++;
 		}
 
 		n = 1;
-		//ArrayList<Person> tenants = new ArrayList<Person>();
 		System.out.println("---------------- Tenants: ----------------");
 		while (n <= 10) {
-			
-			
+
 			Tenant tenant = new Tenant();
 			tenant.setName(faker.name().firstName());
 			tenant.setSurname(faker.name().lastName());
-			
 			tenant.setBankAccount(new BankAccount(faker.finance().iban(), Utils.randRange(25, 75) * 100));
-
 			Request request = new Request(Utils.randRange(5, 18) * 100,
 					Utils.getRandomBoolean() ? Utils.randRange(8, 10) * 10 : null,
 					Utils.getRandomBoolean() ? Utils.randRange(1, 5) : null,
@@ -64,12 +55,9 @@ public class Test {
 			tenant.setRequestId(request.getId());
 			db.getTenants().put(tenant.getId(), tenant);
 			db.getRequests().put(request.getId(), request);
-
 			System.out.println("\n#" + n + " " + tenant);
 			System.out.println(request);
-
-			n++;	
-					
+			n++;
 		}
 	}
 }
